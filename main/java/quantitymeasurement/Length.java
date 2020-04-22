1ab1fcb0215db583d84458df9cf93dba860ab8c2
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-23 01:24:17 +0530

    ADDED:Code and test to check equality of 1 inch to 1 feet

--- main/java/quantitymeasurement/Length.java
+++ main/java/quantitymeasurement/Length.java
@@ -1,6 +1,10 @@
 package quantitymeasurement;
 
 public class Length {
+
+
+    private static final double FEET_TO_INCH = 12;
+
     public enum Unit {FEET, INCH;}
 
     private final double value;
@@ -10,12 +14,21 @@ public class Length {
         this.value = value;
         this.unit = unit;
     }
+    public boolean compare(Length that) {
+        if (this.unit.equals(that.unit))
+            return this.equals(that.unit);
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
+        return false;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Length feet = (Length) o;
-        return Double.compare(feet.value, value) == 0;
+        Length length = (Length) o;
+        return Double.compare(length.value, value) == 0 && unit == length.unit;
     }
 }
 
