164de335ae1e6a0fab419769d7d217ca075b6dac
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-22 17:42:35 +0530

    ADDED:Code and test case to check Type

--- test/java/quantitymeasurement/QuantityTest.java
+++ test/java/quantitymeasurement/QuantityTest.java
@@ -6,28 +6,35 @@ import org.junit.Test;
 public class QuantityTest {
     @Test
     public void given0FeetAnd0Feet_ShouldReturnEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
+        Length feet2 = new Length(0.0,Length.Unit.FEET);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(0.0,Length.Unit.FEET);
+        Length feet2 = new Length(1.0,Length.Unit.FEET);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenOneNullObject_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = null;
+        Length feet1 = new Length(0.0,Length.Unit.FEET);
+        Length feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenSameReferenceToLength_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0.0);
+        Length feet1 = new Length(0.0,Length.Unit.FEET);
         Assert.assertEquals(feet1, feet1);
+    }
+    @Test
+    public void givenCheckTypeInchAndFeet_ShouldReturnNotEqual(){
+        Length feet1 = new Length(0.0,Length.Unit.FEET);
+        Length inch = new Length(0.1,Length.Unit.INCH);
+        Assert.assertNotEquals(feet1, inch);
+
     }
 }
