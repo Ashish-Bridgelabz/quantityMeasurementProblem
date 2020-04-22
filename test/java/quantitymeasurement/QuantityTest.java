e9f33f9a97648e86c157d02cd6412fa695fb2bfd
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-22 18:07:16 +0530

    ADDED:Code and test case to check equality of to values

--- test/java/quantitymeasurement/QuantityTest.java
+++ test/java/quantitymeasurement/QuantityTest.java
@@ -7,34 +7,41 @@ public class QuantityTest {
     @Test
     public void given0FeetAnd0Feet_ShouldReturnEqual() {
         Length feet1 = new Length(0.0, Length.Unit.FEET);
-        Length feet2 = new Length(0.0,Length.Unit.FEET);
+        Length feet2 = new Length(0.0, Length.Unit.FEET);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
-        Length feet1 = new Length(0.0,Length.Unit.FEET);
-        Length feet2 = new Length(1.0,Length.Unit.FEET);
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
+        Length feet2 = new Length(1.0, Length.Unit.FEET);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenOneNullObject_ShouldReturnFalse() {
-        Length feet1 = new Length(0.0,Length.Unit.FEET);
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
         Length feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenSameReferenceToLength_ShouldReturnTrue() {
-        Length feet1 = new Length(0.0,Length.Unit.FEET);
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
         Assert.assertEquals(feet1, feet1);
     }
-    @Test
-    public void givenCheckTypeInchAndFeet_ShouldReturnNotEqual(){
-        Length feet1 = new Length(0.0,Length.Unit.FEET);
-        Length inch = new Length(0.1,Length.Unit.INCH);
-        Assert.assertNotEquals(feet1, inch);
 
+    @Test
+    public void givenCheckTypeInchAndFeet_ShouldReturnNotEqual() {
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
+        Length inch = new Length(0.1, Length.Unit.INCH);
+        Assert.assertNotEquals(feet1, inch);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_shouldReturnNotEqual() {
+        Length feet1 = new Length(0.0, Length.Unit.FEET);
+        Length feet2 = new Length(1.0, Length.Unit.FEET);
+        Assert.assertNotEquals(feet1, feet2);
     }
 }
