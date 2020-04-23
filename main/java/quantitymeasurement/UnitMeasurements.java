ed42fb2f979c1a3dd765edd777db04e7f7242d13
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-23 19:01:41 +0530

    ADDED:We are taking two value and used  add method

--- main/java/quantitymeasurement/UnitMeasurements.java
+++ main/java/quantitymeasurement/UnitMeasurements.java
@@ -1,14 +1,19 @@
 package quantitymeasurement;
 
-    enum UnitMeasurementS {
-        FEET(12.0), INCH(1.0), YARD(36.0);
+enum UnitMeasurementS {
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
 
-        double baseUnitConversion;
+    double baseUnitConversion;
 
-        UnitMeasurementS(double baseUnitConversion) {
-            this.baseUnitConversion = baseUnitConversion;
-        }
-       public  static boolean compare(Length length1, Length length2) {
-            return Double.compare(length1.value * length1.unit.baseUnitConversion, length2.value * length2.unit.baseUnitConversion) == 0;
-        }
+    UnitMeasurementS(double baseUnitConversion) {
+        this.baseUnitConversion = baseUnitConversion;
     }
+
+    public static boolean compare(Length length1, Length length2) {
+        return Double.compare(length1.value * length1.unit.baseUnitConversion, length2.value * length2.unit.baseUnitConversion) == 0;
+    }
+
+    public static double addTwoLengths(Length length1, Length length2) {
+        return length1.value + length2.value;
+    }
+}
