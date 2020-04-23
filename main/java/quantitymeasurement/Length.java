df9b93f946dda8b0e2fe03a2b012cbe63f047c11
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-23 16:11:57 +0530

    ADDED: Code and test to check equality of all the test cases for comparing length 3ft = 1yd, ift!=1yd,1in != 1yd, 1yd =36in, 1yd=3ft

--- main/java/quantitymeasurement/Length.java
+++ main/java/quantitymeasurement/Length.java
@@ -1,27 +1,16 @@
 package quantitymeasurement;
 
+import java.util.Objects;
+
 public class Length {
 
+    public final UnitMeasurementS unit;
+    public final double value;
 
-    private static final double FEET_TO_INCH = 12;
-
-    public enum Unit {FEET, INCH;}
-
-    private final double value;
-    private Unit unit;
-
-    public Length(Double value, Unit unit) {
-        this.value = value;
+    public Length(UnitMeasurementS unit, double value) {
         this.unit = unit;
+        this.value = value;
     }
-    public boolean compare(Length that) {
-        if (this.unit.equals(that.unit))
-            return this.equals(that.unit);
-        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
-            return Double.compare(this.value * FEET_TO_INCH, that.value) == 0;
-        return false;
-    }
-
 
     @Override
     public boolean equals(Object o) {
